7e7f2861e1fdae167577d59e7f5ce734c8fffa78
danielh13210 <danielh13210@gmail.com>
2026-01-05 11:56:46 +0800

    Allow running as root if setup mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+contains() {
+  local e match="$1"; shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
 if [ -f "/.firstrun" ]; then
     targetuid="$1"
     targetgid="$2"
@@ -8,6 +14,12 @@ if [ -f "/.firstrun" ]; then
     useradd -u "$1" -g "$2" -m ubuntu
     rm -f /.firstrun
 fi
-shift 2
-cd ~ubuntu
-exec sudo -u ubuntu "$@"
+IFS=','
+read -a options <<< "$3"
+shift 3
+if ! contains setup "${options[@]}"; then
+  cd ~ubuntu
+  exec sudo -u ubuntu "$@"
+else
+  exec "$@"
+fi
