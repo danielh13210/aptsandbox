d04becf2a700e63b527abff9d1b874c4e9515b47
danielh13210 <danielh13210@gmail.com>
2026-01-05 11:45:16 +0800

    Ensure container user owns their home

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 if [ -f "/.firstrun" ]; then
     targetuid="$1"
     targetgid="$2"
+    rm -rf /home/ubuntu
     groupadd -g "$2" ubuntu
     useradd -u "$1" -g "$2" -m ubuntu
     rm -f /.firstrun
