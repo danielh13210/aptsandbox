2d4032a00fead8104c69fd2726b2fee192b24f7c
danielh13210 <danielh13210@gmail.com>
2026-01-08 07:58:32 +0800

    Handle GUI variables correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,21 @@
 
 contains() {
   local e match="$1"; shift
-  for e; do [[ "$e" == "$match" ]] && return 0; done
+  for e; do [[ "$e" == "$match" ]] || [[ "$e" =="$match="* ]] && return 0; done
   return 1
 }
+
+valueof() {
+  local e match="$1"; shift
+  for e; do
+    if [[ "$e" =="$match="* ]]; then
+      echo "$e" | cut -d '=' -f2-
+      return 0
+    fi
+  done
+  return 1
+}
+
 
 if [ -f "/.firstrun" ]; then
     targetuid="$1"
@@ -20,6 +32,13 @@ read -a options <<< "$3"
 shift 3
 if ! contains setup "${options[@]}"; then
   cd ~ubuntu
+  if contains wayland "${options[@]}"; then
+    export WAYLAND_DISPLAY=/wayland
+  fi
+  if contains xorg "${options[@]}"; then
+    export DISPLAY=$(valueof xorg "${options[@]}")
+    export XAUTHORITY=~ubuntu/.Xauthority
+  fi
   exec sudo -u ubuntu "$@"
 else
   exec "$@"
